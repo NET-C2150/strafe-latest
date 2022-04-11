6a81211b0617beff28a0d9730fcb5c121a6dddd6
NET-C2150 <timothyhughest1811@gmail.com>
2022-04-11 10:14:23 -0600

    Update trigger names

--- code/Map/TimerZones.cs
+++ code/Map/TimerZones.cs
@@ -33,32 +33,31 @@ public partial class BaseZone : BaseTrigger
 
 }
 
-[EntityTool("test", "test2")]
-[Library( "surf_linear_start", Description = "Where the timer will start" )]
+[Library( "strafe_linear_start", Description = "Where the timer will start" )]
 public partial class LinearStart : BaseZone
 {
 
 }
 
-[Library( "surf_linear_end", Description = "Where the timer will end" )]
+[Library( "strafe_linear_end", Description = "Where the timer will end" )]
 internal partial class LinearEnd : BaseZone
 {
 
 }
 
-[Library( "surf_linear_checkpoint", Description = "Where the timer will set a checkpoint (linear)" )]
+[Library( "strafe_linear_checkpoint", Description = "Where the timer will set a checkpoint (linear)" )]
 internal partial class LinearCheckpoint : BaseZone
 {
 
 }
 
-[Library( "surf_stage_start", Description = "Where the timer will begin a stage" )]
+[Library( "strafe_stage_start", Description = "Where the timer will begin a stage" )]
 internal partial class StageStart : BaseZone 
 {
 
 }
 
-[Library( "surf_stage_end", Description = "Where the timer will end a stage" )]
+[Library( "strafe_stage_end", Description = "Where the timer will end a stage" )]
 internal partial class StageEnd : BaseZone
 {
 
