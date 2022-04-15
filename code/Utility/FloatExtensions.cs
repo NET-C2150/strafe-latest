12d9d85f6dc57d919aa7a979b3e11b01d45861cf
NET-C2150 <timothyhughest1811@gmail.com>
2022-04-14 19:50:00 -0600

    Update FloatExtensions.cs

--- code/Utility/FloatExtensions.cs
+++ code/Utility/FloatExtensions.cs
@@ -7,8 +7,8 @@ namespace Strafe.Utility;
 internal static class FloatExtensions
 {
 
-	public static string ToTimeMs( this float seconds ) => TimeSpan.FromSeconds( seconds ).ToString( @"m\:ss\.ff" );
-	public static string ToTime( this float seconds ) => TimeSpan.FromSeconds( seconds ).ToString( @"m\:ss" );
+	public static string ToTimeMs( this float seconds ) => TimeSpan.FromSeconds( seconds ).ToString( @"mm\:ss\.ff" );
+	public static string ToTime( this float seconds ) => TimeSpan.FromSeconds( seconds ).ToString( @"mm\:ss" );
 	public static string ToTimeMs( this RealTimeSince rt ) => ToTimeMs( (float)rt );
 	public static string ToTime( this RealTimeSince rt ) => ToTime( (float)rt );
 
