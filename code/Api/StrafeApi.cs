816ad9fac89c2ce4e91ad76c4418c54f501e9178
NET-C2150 <timothyhughest1811@gmail.com>
2022-05-01 10:30:46 -0600

    Add simple http api

---
+++ code/Api/StrafeApi.cs
@@ -0,0 +1,22 @@
+﻿
+using Sandbox.Internal;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Strafe.Api;
+
+internal class StrafeApi
+{
+
+	public static string Endpoint => "https://localhost:7265/api";
+
+	public static async Task<T> Fetch<T>( string controller )
+	{
+		var http = new Http( new System.Uri( $"{Endpoint}/{controller}" ) );
+		var result = await http.GetStringAsync();
+		http.Dispose();
+
+		return JsonSerializer.Deserialize<T>( result );
+	}
+
+}
